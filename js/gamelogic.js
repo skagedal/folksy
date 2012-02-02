466721ef59a328c6e221c89fb1f0c7121b36f208
Simon Kågedal Reimer <skagedal@gmail.com>
2012-02-02 19:57:50 +0100

    Some sketching. Bus work.

---
+++ js/gamelogic.js
@@ -0,0 +1,35 @@
+// "Interfaces"
+
+Stimulus = {
+  id: "<stimulusID>",
+};
+
+StimulusSet = {
+  each: function (op) { }
+};
+
+Relation = {
+  stimulusA: {},
+  stimulusB: {},
+  pStrength: 1
+};
+
+RelationSet = {
+  add: function (stimulusA, stimulusB) { }
+};
+
+User = {
+  getRelation: function (idA, idB) { },
+};
+
+// Game logic
+
+GameLogic = {
+  newGame: function (user) { 
+  },
+  pickSampleStimulus: function () { },
+  pickComparisonStimuli: function () { },
+  respond: function (stimulus) {
+    return isCorrect;
+  }
+  
