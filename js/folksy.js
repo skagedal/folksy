7597fa04508e9a81093ca8405caf5e1f16a6fa09
Simon Kågedal Reimer <skagedal@gmail.com>
2013-03-29 22:01:48 +0100

    Use SoundJS instead of SoundManager2.

--- js/folksy.js
+++ js/folksy.js
@@ -76,16 +76,29 @@ folksy = (function () {
     }
     var F_ = getResource;	
     
+// Sound
+
     function setupSoundManager() {
-	soundManager.useHTML5Audio = true;
+	// createjs.Sound.registerPlugins([createjs.WebAudioPlugin, createjs.FlashPlugin]);
     }
+
+    function createSound(uniqueId, sources) {
+	return createjs.Sound.registerSound(sources.join("|"), uniqueId);
+	// returns a "details" object. has id as "id".
+    }
+
+    function playSound(sound) {
+	createjs.Sound.stop();
+	createjs.Sound.play(sound.id);
+    }
+
+
 
 // Module helpers
 
     var _debugMode = true;
     function setDebugMode(b) {
 	_debugMode = Boolean(b);
-	//soundManager.debugMode = _debugMode;
     }
     
     function log(s) {
@@ -214,11 +227,7 @@ folksy = (function () {
         loadIm("imageSelect", "image_select_src");
     
         if(stimulus.hasOwnProperty("sound_srcs")) {
-            stimulus.sound = soundManager.createSound({
-                id: stimulus.id,
-                url: stimulus.sound_srcs,
-                autoLoad: true
-            });
+            stimulus.sound = createSound(stimulus.id, stimulus.sound_srcs);
         }
     }
 
@@ -246,11 +255,8 @@ folksy = (function () {
 	    for (var j = 0; j < sound.sound_srcs.length; j++) {
 		sound.sound_srcs[j] = HREF_PREFIX + sound.sound_srcs[j];
 	    }
-	    sound.sound = soundManager.createSound({
-		id: sound.sound_srcs[0],
-		url: sound.sound_srcs,
-		autoLoad: true
-	    });
+	    sound.sound = createSound(sound.sound_srcs[0], sound.sound_srcs);
+
 	}
     }
 
@@ -268,11 +274,6 @@ folksy = (function () {
 	game._imageRequestsSent = true;
     }
 
-
-    function playSound(audio) {
-	soundManager.stopAll();
-	audio.play();
-    }
 
     function rewardImages(game) {
 	return util.pluck(game.rewards.images, 'image');
@@ -331,7 +332,7 @@ folksy = (function () {
 
     function incorrectAnswer(game) {
 	if (game._isInQuestion) {
-	    soundManager.play('fel');
+	    // soundManager.play('fel');
 	}
     }
 
@@ -555,12 +556,9 @@ folksy = (function () {
 	});
 	
 	$(document).ready(function() {
-
-            soundManager.onready(function() {
-		createElements(game);
-		loadGameData(game);
-		$("#credits").append(jsonData.credits || '');
-            });
+	    createElements(game);
+	    loadGameData(game);
+	    $("#credits").append(jsonData.credits || '');
 	});
 	
     }
