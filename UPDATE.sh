b7e74c987329fd2b6ce19f967840930e106c1b95
Simon Kågedal Reimer <skagedal@gmail.com>
2013-03-29 22:32:09 +0100

    Better, simpler way of updating documentation. Updates to TODO.

--- UPDATE.sh
+++ UPDATE.sh
@@ -3,8 +3,9 @@
 # Lame way of updating documentation
 # git cherry-pick <commit> might be a better solution
 
-git checkout master -- ./
-git reset ./
-git checkout gh-pages -- .gitignore
-git clean -fd
-git commit -av
+# git checkout master -- ./
+# git reset ./
+# git checkout gh-pages -- .gitignore
+# git clean -fd
+# git commit -av
+git checkout master doc/TODO.txt doc/framework.txt
