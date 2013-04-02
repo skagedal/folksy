07131c617de40b2434aa11cdc3e3eaabe8f410d3
Simon Kågedal Reimer <skagedal@gmail.com>
2013-04-02 08:41:27 +0200

    We also need to register HTMLAudioPlugin. Add WebAudioPlugin also since it seems recommended, although I'm not sure what the benifits are compared to just using HTMLAudioPlugin.

--- js/folksy.js
+++ js/folksy.js
@@ -99,7 +99,10 @@ folksy = (function () {
     function setupSoundManager() {
 	// createjs.WebAudioPlugin
 	createjs.FlashPlugin.BASE_PATH = "../swf/"
-	createjs.Sound.registerPlugins([createjs.FlashPlugin]);
+	createjs.Sound.registerPlugins([
+	    createjs.WebAudioPlugin, 
+	    createjs.HTMLAudioPlugin,
+	    createjs.FlashPlugin]);
     }
 
     function createSound(uniqueId, sources) {
